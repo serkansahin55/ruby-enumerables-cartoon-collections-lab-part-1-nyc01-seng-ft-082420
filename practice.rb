60464c688e56cd8a471443a4a4496fbe418f4883
Serkan Sahin <ssahin55@outlook.com>
2020-08-23 22:07:29 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,8 +1,8 @@
-chapter_titles = [ "1. The Beginning", "2. The Middle", "3. The End"]
+# chapter_titles = [ "1. The Beginning", "2. The Middle", "3. The End"]
 
-chapter_titles.each do |title|
-  puts title
-end
+# chapter_titles.each do |title|
+#   puts title
+# end
 
 
 [1, 2, 3, 4].count do |num|
