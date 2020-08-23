f040e8a58efefe5da33d9ef59d8d475fb4164ad2
Serkan Sahin <ssahin55@outlook.com>
2020-08-23 22:10:51 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,18 +1,26 @@
-# chapter_titles = [ "1. The Beginning", "2. The Middle", "3. The End"]
+# # chapter_titles = [ "1. The Beginning", "2. The Middle", "3. The End"]
 
-# chapter_titles.each do |title|
-#   puts title
+# # chapter_titles.each do |title|
+# #   puts title
+# # end
+
+
+# [1, 2, 3, 4].count do |num|
+#   puts num.even? 
 # end
 
+# total = 0
+# [1, 2, 3, 4].each do |num|
+#   if num.even?
+#     total += 1 
+#   end
+# end
+# p total
 
-[1, 2, 3, 4].count do |num|
-  puts num.even? 
-end
 
-total = 0
-[1, 2, 3, 4].each do |num|
-  if num.even?
-    total += 1 
-  end
-end
-p total
+
+oppressed_workers = ["dopey", "sneezy", "happy", "angry", "doc", "lemonjello", "sleepy"]
+oppressed_workers.each do |oppressed_workers|
+   puts "#{oppressed_workers.capitalize} wants to start a union!"
+ end
+ 
