9d1ebff8a242b09be4dd566273b351afc4da89d6
Serkan Sahin <ssahin55@outlook.com>
2020-08-23 22:07:07 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -3,3 +3,16 @@ chapter_titles = [ "1. The Beginning", "2. The Middle", "3. The End"]
 chapter_titles.each do |title|
   puts title
 end
+
+
+[1, 2, 3, 4].count do |num|
+  num.even 
+end
+
+total = 0
+[1, 2, 3, 4].each do |num|
+  if num.even?
+    total += 1 
+  end
+end
+total
